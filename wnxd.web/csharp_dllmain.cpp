c139fc19541eb8c18c8876ef938de68dc15f9b9a
为你芯冻 <514543271@qq.com>
2015-10-20 16:58:23 +0800

    采用api方式读取路径

--- wnxd.web/csharp_dllmain.cpp
+++ wnxd.web/csharp_dllmain.cpp
@@ -4,10 +4,14 @@
 using namespace wnxd::Web;
 using namespace System::IO;
 using namespace System::Text;
+using namespace System::Runtime::InteropServices;
 
 void CSharp_DllMain(HMODULE h, DWORD reasonForCall, void* resv)
 {
-	String^ path = Path::GetDirectoryName(Assembly::GetExecutingAssembly()->Location);
+	TCHAR pDllFileName[MAX_PATH];
+	GetModuleFileName(h, pDllFileName, MAX_PATH);
+	IntPtr lpstr = (IntPtr)&pDllFileName;
+	String^ path = Path::GetDirectoryName(Marshal::PtrToStringUni(lpstr));
 	path += "\\__AssemblyInfo__.ini";
 	if (File::Exists(path))
 	{
--- wnxd.web/dllmain.cpp
+++ wnxd.web/dllmain.cpp
@@ -12,6 +12,7 @@ DWORD WINAPI thread(void* info)
 {
 	Info* i = (Info*)info;
 	CSharp_DllMain(i->h, i->reasonForCall, i->resv);
+	free(i);
 	return 0;
 }
 
@@ -19,11 +20,11 @@ extern "C" BOOL APIENTRY DllMain(HMODULE h, DWORD reasonForCall, void* resv)
 {
 	if (reasonForCall == DLL_PROCESS_ATTACH)
 	{
-		Info info;
-		info.h = h;
-		info.reasonForCall = reasonForCall;
-		info.resv = resv;
-		CloseHandle(CreateThread(0, 0, thread, &info, 0, 0));
+		Info *info = (Info*)malloc(sizeof(Info));
+		info->h = h;
+		info->reasonForCall = reasonForCall;
+		info->resv = resv;
+		CloseHandle(CreateThread(0, 0, thread, info, 0, 0));
 	}
 	return TRUE;
 }
